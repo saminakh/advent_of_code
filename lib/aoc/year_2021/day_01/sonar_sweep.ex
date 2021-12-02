3269dcf4574300d0c91859d8fb082f7052221ab9
Samina Khan <samina@township.agency>
2021-12-01 17:59:42 -0700

    feat: day 1 complete

--- lib/aoc/year_2021/day_01/sonar_sweep.ex
+++ lib/aoc/year_2021/day_01/sonar_sweep.ex
@@ -1,3 +1,27 @@
 defmodule Aoc.Year2021.Day01.SonarSweep do
+  @spec part_1(list(integer)) :: integer
+  def part_1(input) do
+    count_increase(input, 0)
+  end
 
+  @spec part_1(list(integer)) :: integer
+  def part_2(input) do
+    count_sum_increase(input, 0)
+  end
+
+  defp count_increase([_a], count), do: count
+
+  defp count_increase([a, b | remaining], count) when b > a,
+    do: count_increase([b | remaining], count + 1)
+
+  defp count_increase([_a, b | remaining], count),
+    do: count_increase([b | remaining], count)
+
+  defp count_sum_increase([_a, _b, _c], count), do: count
+
+  defp count_sum_increase([a, b, c, d | remaining], count) when b + c + d > a + b + c,
+    do: count_sum_increase([b, c, d | remaining], count + 1)
+
+  defp count_sum_increase([_a, b, c, d | remaining], count),
+    do: count_sum_increase([b, c, d | remaining], count)
 end
--- test/aoc/year_2021/day_01/sonar_sweep_test.exs
+++ test/aoc/year_2021/day_01/sonar_sweep_test.exs
@@ -7,14 +7,14 @@ defmodule Aoc.Year2021.Day01.SonarSweepTest do
   describe "part_1/1" do
     @tag day: 01, year: 2021, as_int_list: true
     test "input", %{input: input} do
-      assert input |> Day01.part_1() == 1548
+      assert input |> Day01.part_1() == 1722
     end
   end
 
   describe "part_2/1" do
     @tag day: 01, year: 2021, as_int_list: true
     test "input", %{input: input} do
-      assert input |> Day01.part_2() == 1589
+      assert input |> Day01.part_2() == 1748
     end
   end
 end
